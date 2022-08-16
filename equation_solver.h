abe6e23aace7c617510e715923bfaa17fe0b4aa0
foxido <git@forestfox.tk>
2022-08-16 08:06:19 +0300

    Разделил код на два файла

---
+++ equation_solver.h
@@ -0,0 +1,7 @@
+#ifndef QUAD_EQUATION_SOLVER_H
+#define QUAD_EQUATION_SOLVER_H
+
+int solve_lin_eq(double k, double b, double *x);
+int solve_quad_eq(double a, double b, double c, double *x1, double *x2);
+
+#endif //QUAD_EQUATION_SOLVER_H
