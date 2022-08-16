4e223b55a772b094860ac47520355e31b66b548d
foxido <git@forestfox.tk>
2022-08-16 22:36:14 +0300

    Вернул input_coeffs к void типу

--- main.cpp
+++ main.cpp
@@ -3,15 +3,16 @@
 #include <cerrno>
 #include "equation_solver.h"
 
-int input_coeffs(double *a, double *b, double *c);
+void input_coeffs(double *a, double *b, double *c);
 
 
 int main()
 {
     double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
 
-    if (input_coeffs(&a, &b, &c) != 0) {
-        perror("Не удалось считать коэффициенты по причине");
+    input_coeffs(&a, &b, &c);
+    if (errno != 0) {
+        perror("Не удалось считать коэффициенты");
         return -1;
     }
 
@@ -19,7 +20,7 @@ int main()
 
     // Обрабатываем возможные ошибки
     if (n_roots == ERR_SOLVE) {
-        perror("Не удалось решить уравнение по причине");
+        perror("Не удалось решить уравнение");
         return -1;
     } else {
         // В противном случае используем стандартный вывод решений
@@ -30,30 +31,35 @@ int main()
 
 /**
  * Считывает коэффициенты из stdin и записывает в переданные переменные. При неудаче переспрашивает
- * В случае ошибки ввода (EOF) возвращает ненулевое значение и устанавливает errno
+ * В случае ошибки считывания устанавливает errno != 0
  */
-int input_coeffs(double *a, double *b, double *c)
+void input_coeffs(double *a, double *b, double *c)
 {
     assert(a != nullptr && b != nullptr && c != nullptr);
     double *coeffs[3] = {a, b, c};
     const char *prompts[3] = {"a = ", "b = ", "c = "};
+    errno = 0;
+    int scan_res = 0;
 
     printf("Введите коэффициенты уравнения ax^2 + bx + c = 0\n");
     for (int i = 0; i < 3;) {
         printf("%s", prompts[i]);
 
-        switch (scanf("%lf", coeffs[i])) {
+        scan_res = scanf("%lf", coeffs[i]);
+        if (errno != 0) {
+            return;
+        }
+        switch (scan_res) {
             case 1: // Считалость правильно
                 i++;
                 break;
-            case EOF: // Произошла ошибка ввода
+            case EOF: // Пользователь оборвал ввод
                 errno = EIO;
-                return -1;
+                return;
             default:
                 while (getchar() != '\n');
                 printf("Неправильный ввод, пожалуйста, введите число\n");
                 break;
         }
     }
-    return 0;
 }
